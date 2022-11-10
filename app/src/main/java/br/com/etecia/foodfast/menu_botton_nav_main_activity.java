c75afe2f4ac5a12ae75cc517231db5c6d17365ca
Laboratorio-Info <g_gabrielvictor@hotmail.com>
2022-11-10 20:04:25 -0300

    att3.0

---
+++ app/src/main/java/br/com/etecia/foodfast/menu_botton_nav_main_activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.foodfast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_botton_nav_main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_botton_nav_main_layout);
+    }
+}
