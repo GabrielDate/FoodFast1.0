6078763747bdb8d503e59216c48e59dfa3be1dc9
Laboratorio-Info <g_gabrielvictor@hotmail.com>
2022-11-17 19:55:26 -0300

    commit 17/11 fragment

--- app/src/main/java/br/com/etecia/foodfast/PorcoesFragment.java
+++ app/src/main/java/br/com/etecia/foodfast/PorcoesFragment.java
@@ -7,8 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PorcoesFragment extends Fragment {
+    //variaveis
+    EditText edtPoroes;
+    Button btnEscolha;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,6 +21,8 @@ public class PorcoesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_porcoes, container, false);
 
+        edtPoroes = view.findViewById(R.id.edtPorcoes);
+        btnEscolha = view.findViewById(R.id.btnEscolha);
 
 
         //digitar o codigo a cima do return
